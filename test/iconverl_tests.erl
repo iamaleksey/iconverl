c54d4821e24b14f61360579a1747c054510bff1e
Eric des Courtis <eric.des.courtis@benbria.ca>
2012-11-10 19:59:07 -0500

    Fixed unit tests

--- test/iconverl_tests.erl
+++ test/iconverl_tests.erl
@@ -3,8 +3,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 open_test() ->
-    ?assertError(badarg, iconverl:open("utf-99", "ucs-34")),
-    ?assert(is_binary(iconverl:open("utf-8", "ucs-2be"))).
+    ?assertEqual({error, unsupported}, iconverl:open("utf-99", "ucs-34")),
+    ?assertEqual({cd, "utf-8", "ucs-2be"}, iconverl:open("utf-8", "ucs-2be")).
 
 conv2_test() ->
     CD = iconverl:open("ucs-2be", "utf-8"),
@@ -12,7 +12,7 @@ conv2_test() ->
     ?assertEqual({error, eilseq}, iconverl:conv(CD, <<129,129>>)).
 
 conv3_test() ->
-    ?assertError(badarg, iconverl:open("utf-99", "ucs-34")),
+    ?assertEqual({error, unsupported}, iconverl:open("utf-99", "ucs-34")),
     ?assertEqual({ok, <<0,$t,0,$e,0,$s,0,$t>>},
                  iconverl:conv("ucs-2be", "utf-8", <<"test">>)),
     ?assertEqual({error, eilseq}, iconverl:conv("ucs-2be", "utf-8", <<129,129>>)).
