941838d23b551c948ad1b47e6aca4f0f9fadd430
Eric des Courtis <eric.des.courtis@benbria.ca>
2013-02-23 02:16:01 -0500

    Fix tests

--- test/iconverl_tests.erl
+++ test/iconverl_tests.erl
@@ -4,10 +4,10 @@
 
 open_test() ->
     ?assertEqual({error, unsupported}, iconverl:open("utf-99", "ucs-34")),
-    ?assertMatch({ok, _}, iconverl:open("utf-8", "ucs-2be")).
+    ?assertEqual({ok, <<>>}, iconverl:conv("ascii", "ascii", <<>>)).
 
 conv2_test() ->
-    {ok, CD} = iconverl:open("ucs-2be", "utf-8"),
+    CD = iconverl:open("ucs-2be", "utf-8"),
     ?assertEqual({ok, <<0,$t,0,$e,0,$s,0,$t>>}, iconverl:conv(CD, <<"test">>)),
     ?assertEqual({error, eilseq}, iconverl:conv(CD, <<129,129>>)).
 
