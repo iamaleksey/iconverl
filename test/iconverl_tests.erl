b4f0252fa8f123e3d3669eafb8da0e69a2ae0669
Eric des Courtis <eric.des.courtis@benbria.ca>
2013-02-22 19:03:46 -0500

    Add tests again

--- test/iconverl_tests.erl
+++ test/iconverl_tests.erl
@@ -2,3 +2,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+open_test() ->
+    ?assertEqual({error, unsupported}, iconverl:open("utf-99", "ucs-34")),
+    ?assertMatch({ok, _}, iconverl:open("utf-8", "ucs-2be")).
+
+conv2_test() ->
+    {ok, CD} = iconverl:open("ucs-2be", "utf-8"),
+    ?assertEqual({ok, <<0,$t,0,$e,0,$s,0,$t>>}, iconverl:conv(CD, <<"test">>)),
+    ?assertEqual({error, eilseq}, iconverl:conv(CD, <<129,129>>)).
+
+conv3_test() ->
+    ?assertEqual({error, unsupported}, iconverl:open("utf-99", "ucs-34")),
+    ?assertEqual({ok, <<0,$t,0,$e,0,$s,0,$t>>},
+                 iconverl:conv("ucs-2be", "utf-8", <<"test">>)),
+    ?assertEqual({error, eilseq}, iconverl:conv("ucs-2be", "utf-8", <<129,129>>)).
+
